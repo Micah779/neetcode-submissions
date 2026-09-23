5bd040e9ec51e83d83b45e956bf54ae2ba3f2810
Micah <141525046+Micah779@users.noreply.github.com>
2026-09-23 14:23:39 -0500

    Add: search-2d-matrix - submission-1

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-1.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        // naive approach would be to check every matrix[col][row]
+        // this would be O(n) time complexity w n being mxn
+        // to get O(log(n)) we would need to half this
+        // we could count the number of rows
+        // starting at the middle rows first val
+        // check if its greater then target
+        // if it is we know it has to be in in first half rows
+        // otherwise second half rows
+        // keep running this binary search algorithm on the matrix
+        // which would result in logn time complexity
+
+        int num_rows = matrix.size();
+        int row_length = matrix[0].size();
+
+        int left = 0;
+        int right = num_rows - 1;
+
+        while (left <= right) {
+            int middle = left + (right - left) / 2; // avoid overflow
+            int row_start = matrix[middle][0];
+            int row_end = matrix[middle][row_length - 1];
+
+            if (target >= row_start && target <= row_end) {
+                for (int i = 0; i < row_length; ++i) {
+                    if (matrix[middle][i] == target) {
+                        return true;
+                    }
+                }
+                return false;
+            } else if (target > row_end) {
+                left = middle + 1;
+            } else {
+                right = middle - 1;
+            }
+        }
+        return false;
+    }
+};
